9a39d7da507292fad7ac77dc5f1d94b8d49282fa
luizadeandrade91 <luizaandrade9124@gmail.com>
2023-09-13 15:38:57 -0300

    Projeto 1

---
+++ scripts.js
@@ -0,0 +1,48 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+    console.log(currencySelect.value)
+    const dolarToday = 4.9
+    const euroToday = 5.2
+
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+}
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+    }
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+    }
+
+
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
